265adaa0b041ef0d978f64abee1900529658e34e
Miguel Angel Martelo Quiroz <miguelmartelo@seven4n.com>
2020-08-06 21:01:08 -0500

    Updates

---
+++ precache-manifest.5f01b11dd562e380f979d24cbf7fae7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384b60dfeb830953e45f68a0f9ed5436",
+    "url": "/spa-imdbMovie/index.html"
+  },
+  {
+    "revision": "68db3b07ed36b7b8c9a1",
+    "url": "/spa-imdbMovie/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "58f08039fd1850acfa67",
+    "url": "/spa-imdbMovie/static/css/main.02dd040c.chunk.css"
+  },
+  {
+    "revision": "68db3b07ed36b7b8c9a1",
+    "url": "/spa-imdbMovie/static/js/2.9c287614.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/spa-imdbMovie/static/js/2.9c287614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f08039fd1850acfa67",
+    "url": "/spa-imdbMovie/static/js/main.78b64e5d.chunk.js"
+  },
+  {
+    "revision": "e31d28441bda7ca99edb",
+    "url": "/spa-imdbMovie/static/js/runtime-main.ca1db749.js"
+  }
+]);
